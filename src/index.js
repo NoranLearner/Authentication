3dcbc77d4c369dca94d52c9087711a31c80a46f5
NoranBESTEAM <nmohammed@be-steam.com>
2024-08-16 14:39:26 +0300

    Authentication With Firebase in React

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+import { HelmetProvider } from "react-helmet-async";
+
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+import { ThemeProvider } from "./context/ThemeContext";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <ThemeProvider>
+        <App/>
+      </ThemeProvider>
+    </HelmetProvider>
+  </React.StrictMode>
+);
+
+serviceWorkerRegistration.register();
